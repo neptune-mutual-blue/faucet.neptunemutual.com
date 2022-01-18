ee552d4a1df76d179614afc28e72a4748af0206f
flashburst <82953782+flashburst@users.noreply.github.com>
2022-01-18 10:34:02 +0530

    Fix CSP issue

--- src/components/SelectNetwork.jsx
+++ src/components/SelectNetwork.jsx
@@ -1,59 +1,73 @@
-import * as RadioGroupPrimitive from "@radix-ui/react-radio-group";
-
 import { useNetwork } from "../context/network";
-import styles from "./RadioGroup.module.css";
-
-const RadioGroup = RadioGroupPrimitive.Root;
-const RadioGroupRadioItem = RadioGroupPrimitive.Item;
-const RadioGroupIndicator = RadioGroupPrimitive.Indicator;
 
 export const SelectNetwork = () => {
   const { network, setNetwork } = useNetwork();
 
+  const handleChange = (ev) => {
+    setNetwork(ev.target.value);
+  };
+
   return (
     <>
       <form onSubmit={(ev) => ev.preventDefault()}>
-        <h3 className="text-xs font-inter font-bold mt-6 text-gray-800">
+        <h3 className="text-xs font-inter font-bold py-3.5 text-gray-800">
           Select a Network
         </h3>
-        <RadioGroup
-          defaultValue="default"
-          aria-label="View density"
-          className="flex font-inter text-gray-800 text-sm mt-4"
-          value={network}
-          onValueChange={setNetwork}
-        >
-          <div className="flex justify-start items-center mr-6">
-            <RadioGroupRadioItem
-              className={styles.radio_item}
+        <div className="sm:flex justify-left items-center sm:space-x-5">
+          <div className="flex justify-center items-center">
+            <input
+              type="radio"
+              className="w-4 h-4 accent-36309D"
+              id="networkChoice3"
+              name="network"
               value="3"
-              id="r3"
+              onChange={handleChange}
+              checked={"3" === network}
+            />
+            <label
+              htmlFor="networkChoice3"
+              className="font-inter text-gray-800 ml-1 text-sm"
             >
-              <RadioGroupIndicator className={styles.radio_indicator} />
-            </RadioGroupRadioItem>
-            <label htmlFor="r3">Ropsten</label>
+              Ropsten
+            </label>
           </div>
-          <div className="flex justify-start items-center mr-6">
-            <RadioGroupRadioItem
-              className={styles.radio_item}
+
+          <div className="flex justify-center items-center">
+            <input
+              type="radio"
+              className="w-4 h-4 accent-36309D"
+              id="networkChoice1"
+              name="network"
               value="97"
-              id="r1"
+              onChange={handleChange}
+              checked={"97" === network}
+            />
+            <label
+              htmlFor="networkChoice1"
+              className="font-inter text-gray-800 ml-1 text-sm"
             >
-              <RadioGroupIndicator className={styles.radio_indicator} />
-            </RadioGroupRadioItem>
-            <label htmlFor="r1">BSC Testnet</label>
+              BSC Testnet
+            </label>
           </div>
-          <div className="flex justify-start items-center mr-6">
-            <RadioGroupRadioItem
-              className={styles.radio_item}
+
+          <div className="flex justify-center items-center">
+            <input
+              type="radio"
+              className="w-4 h-4 accent-36309D"
+              id="networkChoice2"
+              name="network"
               value="80001"
-              id="r2"
+              onChange={handleChange}
+              checked={"80001" === network}
+            />
+            <label
+              htmlFor="networkChoice2"
+              className="font-inter text-gray-800 ml-1 text-sm"
             >
-              <RadioGroupIndicator className={styles.radio_indicator} />
-            </RadioGroupRadioItem>
-            <label htmlFor="r2">Mumbai</label>
+              Mumbai
+            </label>
           </div>
-        </RadioGroup>
+        </div>
       </form>
     </>
   );
