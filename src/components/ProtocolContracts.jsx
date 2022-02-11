6552468154bd3a5c204aea99f1c8edba4938fdb6
rupak-nm <rupak@neptunemutual.com>
2022-02-11 17:21:56 +0545

    protocol contracts updated

--- src/components/ProtocolContracts.jsx
+++ src/components/ProtocolContracts.jsx
@@ -10,23 +10,25 @@ export const ProtocolContracts = ({ addresses }) => {
         Protocol Contracts
       </h3>
       <p className="font-inter max-w-fit">
-        {Object.keys(addresses).map((k, idx) => {
-          return (
-            <Fragment key={k}>
-              <a
-                href={getAddressLink(network, addresses[k])}
-                rel="noreferrer"
-                target="_blank"
-                className="text-36309D hover:text-red-600 underline underline-offset-1 text-sm"
-              >
-                {k}
-              </a>
-              {idx !== Object.keys(addresses).length - 1 && (
-                <span className="text-36309D">, </span>
-              )}
-            </Fragment>
-          );
-        })}
+        {Object.keys(addresses)
+          .sort()
+          .map((k, idx) => {
+            return (
+              <Fragment key={k}>
+                <a
+                  href={getAddressLink(network, addresses[k])}
+                  rel="noreferrer"
+                  target="_blank"
+                  className="text-36309D hover:text-red-600 underline underline-offset-1 text-sm"
+                >
+                  {k}
+                </a>
+                {idx !== Object.keys(addresses).length - 1 && (
+                  <span className="text-36309D">, </span>
+                )}
+              </Fragment>
+            );
+          })}
       </p>
     </>
   );
